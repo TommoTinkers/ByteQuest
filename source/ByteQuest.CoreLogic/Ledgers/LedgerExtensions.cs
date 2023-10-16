cb442695f65f5c88c2d844f3a5fc123c06a00d8d
Andrew Thompson <mnemonix.lstudios@gmail.com>
2023-10-16 20:46:58 +0100

    Made ledger entry recording idempotent.
    The same entry cannot be recorded in the ledger more than once.

--- source/ByteQuest.CoreLogic/Ledgers/LedgerExtensions.cs
+++ source/ByteQuest.CoreLogic/Ledgers/LedgerExtensions.cs
@@ -4,5 +4,12 @@ namespace ByteQuest.CoreLogic.Ledgers;
 
 public static class LedgerExtensions
 {
-	public static GameLedger RecordEntry(this GameLedger ledger, Entry entry) => new(Entries: ledger.Entries.Add(entry));
+	public static GameLedger RecordEntry(this GameLedger ledger, Entry entry)
+	{
+		if (ledger.Entries.Select(e => e.id).Contains(entry.id))
+		{
+			return ledger;
+		}
+		return new GameLedger(Entries: ledger.Entries.Add(entry));
+	}
 }
