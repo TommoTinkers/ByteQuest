388402de4371da5e5a322bdd397ea8b623d4fbad
Andrew Thompson <mnemonix.lstudios@gmail.com>
2023-10-20 16:44:51 +0100

    Moved around entry applying logic

--- source/ByteQuest.CoreLogic/State/GameStateBuilder.cs
+++ source/ByteQuest.CoreLogic/State/GameStateBuilder.cs
@@ -2,7 +2,7 @@ using ByteQuest.CoreLogic.Data;
 using ByteQuest.CoreLogic.Data.Modes;
 using ByteQuest.CoreLogic.Entries;
 using ByteQuest.CoreLogic.Ledgers;
-using ByteQuest.CoreLogic.Ledgers.EventAppliers;
+using ByteQuest.CoreLogic.Ledgers.EntryAppliers;
 
 namespace ByteQuest.CoreLogic.State;
 
@@ -10,29 +10,19 @@ namespace ByteQuest.CoreLogic.State;
 
 public static class GameStateBuilders
 {
-		
+
 	private static readonly Enemy enemy = new("Goblin", 20, 9, 3, 5, 8);
 
 	private static readonly GameState defaultGameState = new(new Player(20, 20, 3, 4, 20), 10, new PlayersTurn(enemy));
-	
+
 	public static GameState CreateFromLedger(GameLedger ledger)
 	{
 		return ledger.Entries switch
 		{
 			[] => defaultGameState,
-			var entries => entries.Aggregate(defaultGameState, ApplyChange)
-		};
-	}
-
-	public static GameState ApplyChange(this GameState gameState, Entry entry)
-	{
-		return entry switch
-		{
-			UpdateSeedEntry updateSeed => updateSeed.Apply(gameState),
-			_ => throw new ArgumentOutOfRangeException(nameof(entry))
+			var entries => entries.Aggregate(defaultGameState, GameStateEntryApplier.ApplyChange)
 		};
 	}
 }
-
 
 
