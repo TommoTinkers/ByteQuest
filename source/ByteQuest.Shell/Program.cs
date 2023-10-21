625ec37457a93dee460b83f6b433aab989d729ad
Andrew Thompson <mnemonix.lstudios@gmail.com>
2023-10-21 17:55:02 +0100

    Rapid prototype battle is now working
    This is a battle between bob the goblin and our player!

--- source/ByteQuest.Shell/Program.cs
+++ source/ByteQuest.Shell/Program.cs
@@ -1,37 +1,102 @@
-﻿Console.WriteLine("Welcome to ByteQuest!");
+﻿using static Turn;
+
+Console.WriteLine("Welcome to ByteQuest!");
 
 
 
 var enemy = new { Name = "Bob The Goblin", Health = 10u, Strength = 5u, Accuracy = 5u, Evasion = 5u, Defence = 5u };
 
-var player = new { Health = 10u, Strengh = 5u, Accuracy = 5u, Evasion = 5u, Defence = 5u};
+var player = new { Health = 10u, Strength = 5u, Accuracy = 5u, Evasion = 5u, Defence = 5u};
 
 
 Console.WriteLine($"You are accosted by {enemy.Name}");
 
-var seed = 10;
+var seed = Random.Shared.Next();
+
+var turn = Player;
+
 while (true)
 {
-	Console.WriteLine("What do you want to do?");
-	var input = GetLine();
-
-	switch (input.ToLowerInvariant())
+	if (turn == Player)
 	{
-		case "attack":
-			Console.WriteLine($"You attack {enemy.Name}");
-			var requiredPercentile = 1 - (player.Accuracy / enemy.Evasion);
-			(var roll, seed) = RollPercentile(seed);
+		Console.WriteLine($"Your health: [{player.Health}]. Enemy health: [{enemy.Health}]");
+		Console.WriteLine("It is your turn. What do you want to do.");
+		
+		var input = GetLine();
 
-			if (roll >= requiredPercentile)
+		switch (input.ToLowerInvariant())
+		{
+			case "attack":
+				Console.WriteLine($"You attack {enemy.Name}");
+				var requiredPercentile = 1 - (player.Accuracy / enemy.Evasion);
+				(var roll, seed) = RollPercentile(seed);
+
+				if (roll >= requiredPercentile)
+				{
+					Console.WriteLine($"You managed to hit {enemy.Name}");
+
+					(var damageRoll, seed) = RollPercentile(seed);
+					(var defenceRoll, seed) = RollPercentile(seed);
+					var damageForce = (uint)(damageRoll * player.Strength);
+					var blockingForce = (uint)(defenceRoll * enemy.Defence);
+					var damage = blockingForce > damageForce ? 0 : damageForce - blockingForce;
+					
+					var damageDealt = Math.Min(enemy.Health, damage);
+
+					Console.WriteLine($"You did {damageDealt} points of damage to {enemy.Name}");
+					enemy = enemy with { Health = enemy.Health - damageDealt };
+				}
+				else
+				{
+					Console.WriteLine($"You missed!");
+				}
+
+				turn = Enemy;
+
+				if (enemy.Health == 0)
+				{
+					Console.WriteLine($"You defeated {enemy.Name}");
+					return;
+				}
+				break;
+
+			default:
+				Console.WriteLine("I dont understand what you want");
+				break;
+		}
+	}
+	else
+	{
+		Console.WriteLine($"{enemy.Name} tries to attack you!");
+		var requiredPercentile = 1 - (enemy.Accuracy / player.Evasion);
+		(var roll, seed) = RollPercentile(seed);
+		if (roll >= requiredPercentile)
+		{
+			Console.WriteLine($"You have been hit!");
+
+			(var damageRoll, seed) = RollPercentile(seed);
+			(var defenceRoll, seed) = RollPercentile(seed);
+			var damageForce = (uint)(damageRoll * enemy.Strength);
+			var blockingForce = (uint)(defenceRoll * player.Defence);
+			var damage = blockingForce > damageForce ? 0 : damageForce - blockingForce;
+					
+			var damageDealt = Math.Min(player.Health, damage);
+
+			Console.WriteLine($"You suffered {damageDealt} points of damage.");
+			player = player with { Health = player.Health - damageDealt };
+
+			if (player.Health == 0)
 			{
-				Console.WriteLine($"You managed to hit {enemy.Name}");
+				Console.WriteLine($"You died.");
+				return;
 			}
-			break;
-		
-		default:
-			Console.WriteLine("I dont understand what you want");
-			break;
-		
+		}
+		else
+		{
+			Console.WriteLine($"He missed!");
+		}
+
+		turn = Turn.Player;
 	}
 }
 
@@ -53,4 +118,10 @@ static (double Percentile, int NewSeed) RollPercentile(int seed)
 {
 	var random = new Random(seed);
 	return (random.NextDouble(), random.Next());
+}
+
+internal enum Turn
+{
+	Player,
+	Enemy
 }
